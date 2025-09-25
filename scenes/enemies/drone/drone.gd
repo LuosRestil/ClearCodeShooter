b32c9252c17522b91a762ed16e6e4552da3be85d
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-09-25 19:16:11 -0400

    finalize drone

--- scenes/enemies/drone/drone.gd
+++ scenes/enemies/drone/drone.gd
@@ -8,15 +8,21 @@ extends CharacterBody2D
 @export var accel := 100
 
 var speed: float
+# TODO use raycast for player detection
+# so we don't try to attack through walls
 var player_in_range := false
 var health := 20
 var damageable := true
+var blast_radius := 600
 
 func _ready() -> void:
 	speed = base_speed
 
 func _process(delta: float) -> void:
-	if animation.is_playing(): return
+	if animation.is_playing():
+		damage_entities()
+		return
+		
 	if player_in_range:
 		look_at(Globals.player_position)
 		speed += accel * delta
@@ -36,6 +42,12 @@ func take_hit() -> void:
 	health -= 10
 	if health <= 0:
 		explode()
+		
+func damage_entities():
+	var targets := get_tree().get_nodes_in_group("ItemContainer") + get_tree().get_nodes_in_group("Entity")
+	for target in targets:
+		var dist = target.global_position.distance_to(global_position)
+		if dist <= blast_radius and "take_hit" in target: target.take_hit()
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	player_in_range = true
